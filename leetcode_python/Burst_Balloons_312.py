ff6153edceca8513a7823f34852b3fa08de7d536
xiaoxiaojiangshang <1607695601@qq.com>
2021-08-06 00:19:21 +0800

    upload number of digit one 233

---
+++ leetcode_python/Burst_Balloons_312.py
@@ -0,0 +1,11 @@
+# -*- ecoding: utf-8 -*-
+# ModuleName: Burst_Balloons_312
+# IDE:PyCharm
+# Author: David
+# Time: 2021/8/6 0:18
+from typing import List
+from common.common_algorithm import Tree
+from common.data_structure import TreeNode
+
+if __name__ == '__main__':
+    a = 1
--- leetcode_python/Number_of_Digit_One_233.py
+++ leetcode_python/Number_of_Digit_One_233.py
@@ -7,9 +7,24 @@ from typing import List
 from common.common_algorithm import Tree
 from common.data_structure import TreeNode
 
+
 class Solution:
+    def to_num(self, string):
+        if len(string) == 0: return 0
+        return int(string)
+
     def countDigitOne(self, n: int) -> int:
-        for index,value in enumerate(n):
+        n = str(n)
+        ret = 0
+        for index, num in enumerate(n):
+            prefix = self.to_num(n[:index])
+            suffix = 10**(len(n)-index-1)
+            ret += prefix*suffix
+            if num>'1': ret += suffix
+            elif num == '1': ret += self.to_num(n[index+1:])+1
+        return ret
+
 
 if __name__ == '__main__':
-    a = 1
+    a = 100
+    print(Solution().countDigitOne(a))
