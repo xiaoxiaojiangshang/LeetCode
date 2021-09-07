a83fe9a2bbb506cecdbe6e2c034d4a042a283bf2
xiaoxiaojiangshang <1607695601@qq.com>
2021-09-08 00:05:23 +0800

    update gitignore

--- common/common_algorithm.py
+++ common/common_algorithm.py
@@ -4,7 +4,10 @@
 # Author: David
 # Time: 2021/8/4 23:59
 from typing import List
+import os
 from data_structure import TreeNode
+import time
+
 class Tree(object):
     def build_tree(self,input_data):
         n = len(input_data)
@@ -35,10 +38,24 @@ class Tree(object):
                 right = None if node.right == None else node.right.val
                 print("father:{} | left:{} | right:{}".format(node.val,left,right))
 
-def generate_topic(string):
+def generate_topic(string,root_path='../leetcode_python/'):
     string_list = string.replace('.','').split(' ')
     string_list = string_list[1:] + [string_list[0]]
-    return '_'.join(string_list)
+    file_name = '_'.join(string_list)
+    if os.path.exists(root_path+file_name+'.py') == False:
+        f = open(root_path+file_name+'.py','w',encoding='utf-8')
+        f.write('# -*- ecoding: utf-8 -*-\n')
+        f.write('# ModuleName: %s\n'%file_name)
+        f.write('# IDE:PyCharm\n# Author: David\n')
+        f.write('# Time: {}\n'.format(time.strftime("%Y-%m-%d %H:%M", time.localtime())))
+        f.write('from typing import List\nfrom common.common_algorithm import Tree\nfrom common.data_structure import TreeNode\n\n\n\n')
+        f.write("if __name__ == '__main__':\n    pass")
+        f.close()
+    else:
+        print("%s file already exits"%file_name)
+    return file_name
+
+
 if __name__ == "__main__":
-    string = "689. Maximum Sum of 3 Non-Overlapping Subarrays"
+    string = "943. Find the Shortest Superstring"
     print(generate_topic(string))
