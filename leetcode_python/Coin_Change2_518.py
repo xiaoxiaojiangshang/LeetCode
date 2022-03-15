d3ce299366e8fa70ae72ce07f78c37f1c46f92ea
xiaoxiaojiangshang <1607695601@qq.com>
2022-03-16 01:26:36 +0800

    upload 518

--- leetcode_python/Coin_Change2_518.py
+++ leetcode_python/Coin_Change2_518.py
@@ -4,12 +4,25 @@
 # Author: David
 # Time: 2022-03-15 00:24
 from typing import List
-#from common.common_algorithm import Tree
-#from common.data_structure import TreeNode
-
+"""
+假设dp[i] 代表组成i 的硬币最少组合，那coins = [1,5] dp[6] = dp[1] + dp[5]?
+不太对，那么假设dp[i][j] 代表 前j 个硬币，保持有序
+那么1,5 或者5,1 就不会重复
+"""
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        pass
+        dp = [0] * (amount+1)
+        dp[0] = 1
+        for coin in coins:
+            for i in range(coin,amount+1):
+                dp[i] += dp[i - coin]
+            # for i in range(1, amount+1):
+            #     if i >= coin:
+            #         dp[i] += dp[i-coin]
+        return dp[amount]
+    ## 可以优化一下
+
+
 
 
 def main_test():
