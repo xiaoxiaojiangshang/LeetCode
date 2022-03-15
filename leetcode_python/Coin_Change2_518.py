1be0d901a91f466434eae386da606ee39eee9950
xiaoxiaojiangshang <1607695601@qq.com>
2022-03-16 01:01:09 +0800

    upload 518

---
+++ leetcode_python/Coin_Change2_518.py
@@ -0,0 +1,22 @@
+# -*- ecoding: utf-8 -*-
+# ModuleName: Coin_Change2_518
+# IDE:PyCharm
+# Author: David
+# Time: 2022-03-15 00:24
+from typing import List
+#from common.common_algorithm import Tree
+#from common.data_structure import TreeNode
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        pass
+
+
+def main_test():
+    amount = 5
+    coins = [1, 2, 5]
+    sl = Solution()
+    print(sl.change(amount,coins))
+
+if __name__ == '__main__':
+    main_test()
