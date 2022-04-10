4a7f24a6fbba9b8bdb2bcdc8c416d39fb60970c5
xiaoxiaojiangshang <1607695601@qq.com>
2022-04-11 01:20:36 +0800

    upload 135 problemm

--- leetcode_python/Candy_135.py
+++ leetcode_python/Candy_135.py
@@ -36,11 +36,50 @@ class Solution:
 
         return rat
 
+    """
+    potin: 一个点的得分只与左右两边的点有关系。
+    status: up, down, equal
+    up: 比前一个大，1,2,3 对于3，需要rating += 3,此时up=3
+    down : 与 up 类似，比前一个小，3,2,1，down =3, 但需要注意一下，1,5,4,3,2 中5
+    的rating:up=2,down = 4,这个需要技巧来消除结果
+    init:
+        up, down, ans = 1, 1, 1
+    example1:
+        2,4,3,2,1   4 的情况下， up=2, down=1. 3 down =2, 
+        down = up , down -1
+        --> down < up down -1
+        --> down >up 2 down =3, 需要加的是3， 这其实是倒过来计算的
+    example:
+    1,2,2,1 相等的时候重新开始
 
+    error1:
+        没有考虑下降后 up 需要等于 1
+    error2:
+        没有考虑上升后 down = 1
+    """
+
+    def candy2(self, ratings: List[int]) -> int:
+        up, down, peak, ans = 1, 1, 1, 1
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i-1]:
+                up += 1
+                ans += up
+                peak = up ## 上升峰顶
+                down = 1 ## error 2
+            elif ratings[i] == ratings[i-1]:
+                up, down = 1, 1
+                ans += 1
+                peak = 1
+            else:
+                down += 1
+                up = 1 # error1
+                ans += [down-1, down][down > peak]
+        return ans
 def main_test():
     sl = Solution()
-    ratings = [1,2,3,1,0,-1,-2]
+    ratings = [5,3,7,3]
     print(sl.candy(ratings))
+    print(sl.candy2(ratings))
 
 
 if __name__ == '__main__':
